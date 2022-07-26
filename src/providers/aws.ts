a4d34c31f747aa8b1f15886e404b42cea99b1495
Alon Noy <alon.noy@algosec.com>
2022-07-26 12:03:29 +0300

    aws configure

--- src/providers/aws.ts
+++ src/providers/aws.ts
@@ -1,11 +1,8 @@
 
 import { config } from 'aws-sdk'; 
-export function loginToAws() {
-    config.getCredentials(function(err) {
-        if (err) console.log(err.stack);
-        // credentials not loaded
-        else {
-          console.log("Region:", config.region);
-        }
-    });
+export async function loginToAws() {
+    return config.update({
+        accessKeyId: 'AKIAZGQIXN2MLBWEHT6Z',
+        secretAccessKey: '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'
+        });
 }
