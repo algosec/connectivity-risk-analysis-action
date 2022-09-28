0de4a05c30ceaa33364bdc5fc58d43309cb50486
Vitaly Landa <vitaly.landa@algosec.com>
2022-09-28 14:32:45 +0300

    hate js

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-
 import { AshCodeAnalysis } from "./code-analysis";
 import { RiskAnalysisFile } from "./common/risk.model";
 import { FrameworkKeys, IFramework } from "./iaas-tools/framework.model";
@@ -15,42 +14,46 @@ export class Main {
   vcsType: VersionControlKeys;
 
   constructor() {
-    this.vcsType = 'github' as VersionControlKeys // Add when supported (process?.env?.VCS ?? 'github') as VersionControlKeys;
-    this.frameworkType = 'terraform' as FrameworkKeys // Add when supported (process?.env?.FRAMEWORK ?? 'terraform') as FrameworkKeys;
+    this.vcsType = "github" as VersionControlKeys; // Add when supported (process?.env?.VCS ?? 'github') as VersionControlKeys;
+    this.frameworkType = "terraform" as FrameworkKeys; // Add when supported (process?.env?.FRAMEWORK ?? 'terraform') as FrameworkKeys;
   }
 
   async run(): Promise<void> {
     try {
-      const vcs: IVersionControl = await new VersionControlService().getInstanceByType(this.vcsType);
-      const framework: IFramework | null = await new FrameworkService().getInstanceByType(
-        this.frameworkType,
-        vcs
-      );
+      const vcs: IVersionControl =
+        await new VersionControlService().getInstanceByType(this.vcsType);
+      const framework: IFramework | null =
+        await new FrameworkService().getInstanceByType(this.frameworkType, vcs);
       const codeAnalyzer = await new AshCodeAnalysis(vcs);
       const isInitilizaed = await codeAnalyzer.init();
       if (isInitilizaed && framework) {
-        let foldersToRunCheck = []
-        foldersToRunCheck = await vcs.checkForDiffByFileTypes(framework.fileTypes);
+        let foldersToRunCheck = [];
+        foldersToRunCheck = await vcs.checkForDiffByFileTypes(
+          framework.fileTypes
+        );
         if (foldersToRunCheck?.length > 0) {
           const filesToAnalyze: RiskAnalysisFile[] = await framework?.check(
             foldersToRunCheck,
             vcs.workDir
           );
-          if (filesToAnalyze?.length > 0 ) {
-            const codeAnalysisResponses = await codeAnalyzer.analyze(filesToAnalyze);
-              await vcs.parseOutput(filesToAnalyze as any, codeAnalysisResponses);
-          }
-          else {
+          if (filesToAnalyze?.length > 0) {
+            const codeAnalysisResponses = await codeAnalyzer.analyze(
+              filesToAnalyze
+            );
+            await vcs.parseOutput(filesToAnalyze as any, codeAnalysisResponses);
+          } else {
             await vcs.parseOutput([], [], "No files to analyze");
           }
         }
       } else {
-        await vcs.parseOutput([], [], "Not Authenticated, please check action's logs");
+        await vcs.parseOutput(
+          [],
+          [],
+          "Not Authenticated, please check action's logs"
+        );
       }
-     
     } catch (_e) {
       throw new Error(_e);
-      
     }
   }
 }
