498362947a7655a5b37482e29a928de8dda5e760
Alon Noy <alon.noy@algosec.com>
2022-10-18 17:07:25 +0300

    CS-7132

--- src/vcs/github.ts
+++ src/vcs/github.ts
@@ -421,9 +421,9 @@ export class Github implements IVersionControl {
 ${risk?.items?.map(item => 
   `<tr>\n
   <td>${item?.vendor}</td>\n
-  <td>${item?.fromPort}</td>\n
-  <td>${item?.toPort}</td>\n
-  <td>${item?.ipProtocol}</td>\n
+  <td>${item?.fromPort ?? ''}</td>\n
+  <td>${item?.toPort ?? ''}</td>\n
+  <td>${item?.ipProtocol ?? ''}</td>\n
   <td>${item?.ipRange ?? ''}</td>\n
   </tr>\n`)?.join('')}                
 </tbody>
