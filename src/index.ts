f9ee6bd9124f30fff7001eed7b04f94dbf442a70
Sara Prager <sara.prager@algosec.com>
2022-11-08 13:59:42 +0200

    Use main flow

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-//import { Main } from "./main";
-//const codeAnalyzer = new Main();
-//codeAnalyzer.run();
+import { Main } from "./main";
+const codeAnalyzer = new Main();
+codeAnalyzer.run();
 
-import { terraform } from "./temp_terraform_try";
-terraform();
+// import { terraform } from "./temp_terraform_try";
+// terraform();
